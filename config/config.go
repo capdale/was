c9dd73a10a5d4753718106de05dbe697d483d7c0
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-09-15 00:25:08 +0900

    feat: config parser

---
+++ config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Mysql Mysql `yaml:"mysql"`
+	Redis Redis `yaml:"redis"`
+	Key   Key   `yaml:"key"`
+}
+
+type Mysql struct {
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Address  string `yaml:"address"`
+	Port     int    `yaml:"port"`
+}
+
+type Redis struct {
+	Address  string `yaml:"address"`
+	Password string `yaml:"password"`
+	Db       int    `yaml:"db"`
+}
+
+type Key struct {
+	Jwtkey string `yaml:"jwtkey"`
+}
+
+func ParseConfig(filepath string) (c *Config, err error) {
+	buf, err := os.ReadFile(filepath)
+	if err != nil {
+		return
+	}
+
+	c = &Config{}
+	err = yaml.Unmarshal(buf, c)
+	return
+}
