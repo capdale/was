7d8d9fb32f455b0e02f68f297fb3ab493f8f1d9d
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-09-15 00:37:01 +0900

    server

--- server/server.go
+++ server/server.go
@@ -1,8 +1,12 @@
 package server
 
 import (
+	"net/http"
 	"time"
 
+	"github.com/capdale/was/api/collect"
+	"github.com/capdale/was/config"
+	"github.com/capdale/was/database"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -22,9 +26,27 @@ func SetupRouter() (r *gin.Engine) {
 		},
 	))
 
-	config, err := parseConfig("config.yaml")
+	config, err := config.ParseConfig("config.yaml")
 	if err != nil {
-		panic("parse config error")
+		panic(err)
+	}
+
+	d, err := database.New(config)
+	if err != nil {
+		panic(err)
+	}
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.AbortWithStatusJSON(http.StatusOK, gin.H{
+			"ok": "ok",
+		})
+	})
+
+	collectAPI := collect.New(d)
+
+	collectRouter := r.Group("/collect")
+	{
+		collectRouter.POST("/", collectAPI.PostCollectHandler)
 	}
 
 	return
