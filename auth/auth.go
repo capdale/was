c5693481e3e198378adae8eb3b7176577a1f08e0
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-09-20 03:51:15 +0900

    feat: token query

--- auth/auth.go
+++ auth/auth.go
@@ -1,5 +1,7 @@
 package auth
 
+import "github.com/gin-gonic/gin"
+
 type database interface {
 }
 
@@ -19,4 +21,21 @@ func (a *Auth) New(database database, store store) *Auth {
 		DB:    database,
 		Store: store,
 	}
+}
+
+func (a *Auth) tokenFromCTX(ctx *gin.Context) string {
+	if token := a.tokenFromQuery(ctx); token != "" {
+		return token
+	} else if token := a.tokenFromXToken(ctx); token != "" {
+		return token
+	}
+	return ""
+}
+
+func (a *Auth) tokenFromQuery(ctx *gin.Context) string {
+	return ctx.Request.URL.Query().Get("token")
+}
+
+func (a *Auth) tokenFromXToken(ctx *gin.Context) string {
+	return ctx.Request.Header.Get("X-MD-Token")
 }
