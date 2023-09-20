39c14b3f2cd23cffdc000ee80e91b20f0a0b5a15
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-09-20 20:17:28 +0900

    model: user

--- model/user.go
+++ model/user.go
@@ -1,1 +1,16 @@
 package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	Username    string
+	AccountType int
+	UUID        uuid.UUID `gorm:"type:varchar(36);uniqueIndex;not null"`
+	Email       string    `gorm:"size:64;uniqueIndex;not null"`
+	CreatedAt   time.Time `gorm:"autoCreateTime"`
+	UpdateAt    time.Time `gorm:"autoUpdateTime"`
+}
