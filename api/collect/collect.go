a40a360b8e164b8441e320b8278f0a3c4ce4371c
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-09-22 11:52:50 +0900

    Validate image data

--- api/collect/collect.go
+++ api/collect/collect.go
@@ -1,8 +1,10 @@
 package collect
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/capdale/was/location"
 	"github.com/gin-gonic/gin"
 )
 
@@ -26,6 +28,31 @@ func (a *CollectAPI) GetCollectection(ctx *gin.Context) {
 }
 
 func (a *CollectAPI) PostCollectHandler(ctx *gin.Context) {
+	geoLocation, err := location.GeoLocationFromCTX(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "no geolocation data",
+		})
+		return
+	}
+
+	b, err := getByteFromCTX(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "no b data",
+		})
+		return
+	}
+
+	if !isValidImage(b) {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "invalid data",
+		})
+		return
+	}
+
+	fmt.Println(*geoLocation)
+
 	ctx.JSON(http.StatusOK, gin.H{
 		"ok": "ok",
 	})
---
+++ api/collect/image.go
@@ -0,0 +1,34 @@
+package collect
+
+import (
+	"bytes"
+	"image/jpeg"
+	"io"
+
+	"github.com/gin-gonic/gin"
+)
+
+func isValidImage(b *[]byte) bool {
+	img, err := jpeg.DecodeConfig(bytes.NewReader(*b))
+	if err != nil {
+		return false
+	}
+	if img.Height != 3000 {
+		return false
+	}
+	if img.Width != 3000 {
+		return false
+	}
+	return true
+}
+
+func getByteFromCTX(ctx *gin.Context) (*[]byte, error) {
+	body := ctx.Request.Body
+	defer body.Close()
+
+	b, err := io.ReadAll(body)
+	if err != nil {
+		return nil, err
+	}
+	return &b, nil
+}
