6202060a297386c3e64260a8c972acff895cd472
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-09-27 01:01:09 +0900

    graceful shutdown

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/capdale/was/config"
 	"github.com/capdale/was/server"
 )
@@ -15,7 +23,33 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer c.Close()
 
-	r.Run(config.Service.Address)
+	srv := &http.Server{
+		Addr:    config.Service.Address,
+		Handler: r,
+	}
+
+	go func() {
+		defer c.Close()
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 2)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 10 seconds")
+	}
+	log.Println("Server exiting")
 }
