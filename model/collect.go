113f35416e502afb7b74123d7ab8c42245ae8049
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-10-02 19:43:18 +0900

    Model: collection

--- model/collect.go
+++ model/collect.go
@@ -1,7 +1,18 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Collection struct {
-	UserUUID uuid.UUID `gorm:""`
+	UserUUID        uuid.UUID `gorm:"type:varchar(36);not null"`
+	CollectionIndex int64     `gorm:"not null"`
+	Longtitude      float64   `gorm:"not null"`
+	Latitude        float64   `gorm:"not null"`
+	Altitude        float64   `gorm:"not null"`
+	Accuracy        float64   `gorm:"not null"`
+	OriginAt        time.Time `gorm:"not null"`
+	CreatedAt       time.Time `gorm:"autoCreateTime"`
 }
