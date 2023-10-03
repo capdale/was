955f10162aa7b5f75d3e5f04bc50901ebc49c782
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-10-03 17:15:38 +0900

    Image queue, soft delete, explicit primary key

--- model/image_queue.go
+++ model/image_queue.go
@@ -4,9 +4,11 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type ImageQueue struct {
+	ID         uint      `gorm:"primaryKey"`
 	UUID       uuid.UUID `gorm:"type:varchar(36);not null"`
 	UserUUID   uuid.UUID `gorm:"type:varchar(36);not null"`
 	Longtitude float64   `gorm:"not null"`
@@ -14,4 +16,5 @@ type ImageQueue struct {
 	Altitude   float64   `gorm:"not null"`
 	Accuracy   float64   `gorm:"not null"`
 	CreatedAt  time.Time `gorm:"autoCreateTime"`
+	DeletedAt  gorm.DeletedAt
 }
