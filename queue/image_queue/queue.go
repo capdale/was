8a11984b69a0e5067cd8ab73aca2abef33edb0a5
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-10-03 17:16:55 +0900

    Image queue processing, get -> pop, soft delete, recover, delete, remove tmp file

--- queue/image_queue/queue.go
+++ queue/image_queue/queue.go
@@ -9,30 +9,31 @@ import (
 	"time"
 
 	"github.com/capdale/was/model"
+	rpcservice "github.com/capdale/was/rpc"
 	rpc_protocol "github.com/capdale/was/rpc/proto"
 	"github.com/google/uuid"
 )
 
 type database interface {
-	GetImageQueues(n int) (*[]*model.ImageQueue, error)
+	PopImageQueues(n int) (*[]model.ImageQueue, error)
+	RecoverImageQueue(index uint) error
+	DeleteImageQueues(index uint) error
 }
 
 type ImageQueue struct {
 	Duration               time.Duration
 	DB                     database
-	ImageClassifierClients *[]*rpc_protocol.ImageClassifyClient
+	imageClassifierSevices *[]*rpcservice.ImageClassify
 	once                   sync.Once
 }
 
 // change to imageSub (divide)
-type imageSub struct {
-	ImageClassifierClient *rpc_protocol.ImageClassifyClient
-}
 
-func New(d database, t time.Duration) *ImageQueue {
+func New(d database, t time.Duration, imageClassifierSevices *[]*rpcservice.ImageClassify) *ImageQueue {
 	return &ImageQueue{
-		Duration: t,
-		DB:       d,
+		Duration:               t,
+		DB:                     d,
+		imageClassifierSevices: imageClassifierSevices,
 	}
 }
 
@@ -43,28 +44,28 @@ func (q *ImageQueue) Run(ctx *context.Context) {
 }
 
 func (q *ImageQueue) mainRoutine(ctx *context.Context) {
-	ch := make(chan *model.ImageQueue, len(*q.ImageClassifierClients))
+	ch := make(chan *model.ImageQueue, len(*q.imageClassifierSevices))
 	ticker := time.NewTicker(q.Duration)
-	maxChannelN := len(*q.ImageClassifierClients)
+	maxChannelN := len(*q.imageClassifierSevices)
 	defer func() {
 		ticker.Stop()
 	}()
 
-	for _, imageClient := range *q.ImageClassifierClients {
-		go q.subRoutine(ctx, ch, imageClient)
+	for _, imageService := range *q.imageClassifierSevices {
+		go q.subRoutine(ctx, ch, imageService)
 	}
 
 	for {
 		select {
 		case <-ticker.C:
-			images, err := q.DB.GetImageQueues(maxChannelN)
+			images, err := q.DB.PopImageQueues(maxChannelN)
 			// fatal error, need alert
 			if err != nil {
 				fmt.Printf("Image Queue Error: %s", err.Error())
 				break
 			}
 			for _, image := range *images {
-				ch <- image
+				ch <- &image
 			}
 		case <-(*ctx).Done():
 			return
@@ -72,7 +73,7 @@ func (q *ImageQueue) mainRoutine(ctx *context.Context) {
 	}
 }
 
-func (q *ImageQueue) subRoutine(ctx *context.Context, ch chan *model.ImageQueue, client *rpc_protocol.ImageClassifyClient) {
+func (q *ImageQueue) subRoutine(ctx *context.Context, ch chan *model.ImageQueue, service *rpcservice.ImageClassify) {
 	for {
 		select {
 		case <-(*ctx).Done():
@@ -80,17 +81,37 @@ func (q *ImageQueue) subRoutine(ctx *context.Context, ch chan *model.ImageQueue,
 		case image := <-ch:
 			imageBytes, err := q.getImageAsByte(&image.UUID)
 			if err != nil {
-				// log fatal, can't get image from local storage
+				// log get image failed, err, need to trace in 10 hours
+				if err := q.DB.RecoverImageQueue(image.ID); err != nil {
+					// log recover failed, err
+					fmt.Println("warning") // TODO: change to log
+				}
 				break
 			}
-			reply, err := (*client).ClassifyImage(*ctx, &rpc_protocol.ImageClassifierRequest{Image: *imageBytes})
+			reply, err := (*service.Client).ClassifyImage(*ctx, &rpc_protocol.ImageClassifierRequest{Image: *imageBytes})
 			if err != nil {
-				// log fatal rpc server works bad
+				// log rpc server works bad, critical err
+				if err := q.DB.RecoverImageQueue(image.ID); err != nil {
+					// log recover failed, err
+					fmt.Println("warning") // TODO: change to log
+				}
 				break
+			}
+			if err := q.DB.DeleteImageQueues(image.ID); err != nil {
+				// log delete permanent error
+				fmt.Println("delete data error") // TODO: change to log
 			}
 			fmt.Println(reply.ClassIndex)
 			// move to external storage
 			// pub, classify event to subs
+			// ...
+
+			// final task, remove file from storage
+			err = q.RemoveImageFile(&image.UUID)
+			if err != nil {
+				// log this, not fatal, warning
+				fmt.Println("remove image file error") // TODO: chane to log
+			}
 		}
 	}
 }
@@ -98,4 +119,8 @@ func (q *ImageQueue) subRoutine(ctx *context.Context, ch chan *model.ImageQueue,
 func (q *ImageQueue) getImageAsByte(imagePath *uuid.UUID) (*[]byte, error) {
 	bytes, err := os.ReadFile(path.Join("./secret", imagePath.String()))
 	return &bytes, err
+}
+
+func (q *ImageQueue) RemoveImageFile(imagePath *uuid.UUID) error {
+	return os.Remove(imagePath.String())
 }
