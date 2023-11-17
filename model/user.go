fbca90f0068a700e18162dd288b54c214f5dc295
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-11-17 09:30:30 +0900

    user: refresh token

--- model/user.go
+++ model/user.go
@@ -14,3 +14,11 @@ type User struct {
 	CreatedAt   time.Time `gorm:"autoCreateTime"`
 	UpdateAt    time.Time `gorm:"autoUpdateTime"`
 }
+
+type Token struct {
+	// this token is same as jwt token, write when token generated, delete when token blacklist, query when refresh request comes in
+	UUID      uuid.UUID `gorm:"type:varchar(36);uniqueIndex;not null"`
+	Token     string    `gorm:"type:varchar(100);not null"` // need type tuning
+	ExpireAt  time.Time `gorm:"index;,sort:desc"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+}
