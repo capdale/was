5ddab77e25eefb3bba1ea8124bacb3863abcfc02
devhoodit <hoodit@naver.com>
2024-01-30 17:55:58 +0900

    create, query, remove tokens with transaction

---
+++ database/auth.go
@@ -0,0 +1,93 @@
+package database
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/capdale/was/model"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+var ErrNoAffectedRow = errors.New("there is no specific row")
+
+func (d *DB) SaveToken(userUUID *uuid.UUID, tokenString string, refreshToken *[]byte, agent *string) error {
+	UID, err := uuid.NewRandom()
+	if err != nil {
+		return err
+	}
+	BUID, err := UID.MarshalBinary()
+	if err != nil {
+		return err
+	}
+
+	return d.DB.Create(&model.Token{
+		ID:           BUID,
+		UserUUID:     *userUUID,
+		Token:        tokenString,
+		RefreshToken: *refreshToken,
+		UserAgent:    *agent,
+		ExpireAt:     time.Now().Add(time.Hour * 24),
+	}).Error
+}
+
+func (d *DB) PopTokenByRefreshToken(refreshToken *[]byte, transactionF func(string) error) (tokenString *string, err error) {
+	t := &model.Token{}
+	d.DB.Transaction(func(tx *gorm.DB) error {
+		if err = tx.Select("token").Where("refresh_token = ?", refreshToken).Find(t).Error; err != nil {
+			return err
+		}
+		if err = tx.Debug().Select("token").Where("refresh_token = ?", refreshToken).Clauses(clause.Returning{}).Delete(t).Error; err != nil {
+			return err
+		}
+		fmt.Println(t)
+		tokenString = &t.Token
+		return transactionF(t.Token)
+	})
+	return
+}
+
+func (d *DB) IfTokenExistRemoveElseErr(tokenString string, until time.Duration, blackToken func(string, time.Duration) error) error {
+	return d.DB.Transaction(func(tx *gorm.DB) error {
+		result := tx.Select("").Where("token = ?", tokenString).Delete(&model.Token{})
+		if result != nil {
+			return tx.Error
+		}
+		if tx.RowsAffected < 1 {
+			return ErrNoAffectedRow
+		}
+
+		if err := blackToken(tokenString, until); err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+func (d *DB) QueryAllTokensByUserUUID(userUUID *uuid.UUID) (*[]*model.Token, error) {
+	tokenMs := []model.Token{}
+	if err := d.DB.Where("user_uuid = ?", userUUID).Find(&tokenMs).Error; err != nil {
+		return nil, err
+	}
+
+	deleteTokens := []string{}
+	tokens := []*model.Token{}
+	curTime := time.Now()
+
+	for _, token := range tokenMs {
+		if token.ExpireAt.Before(curTime) {
+			tokens = append(tokens, &token)
+		} else {
+			deleteTokens = append(deleteTokens, token.Token)
+		}
+	}
+	go d.RemoveTokens(&deleteTokens)
+	return &tokens, nil
+}
+
+func (d *DB) RemoveTokens(tokenStrings *[]string) error {
+	err := d.DB.Where("token = ?", tokenStrings).Delete(&model.Token{}).Error
+	return err
+}
