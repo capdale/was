d4894e157e4c5ac6164c3b234e40a6a22be54b39
devhoodit <hoodit@naver.com>
2024-01-30 17:57:49 +0900

    api model

--- model/collect.go
+++ model/collect.go
@@ -14,6 +14,15 @@ type Collection struct {
 	Latitude        float64   `gorm:"not null"`
 	Altitude        float64   `gorm:"not null"`
 	Accuracy        float64   `gorm:"not null"`
-	OriginAt        time.Time `gorm:"not null"`
-	CreatedAt       time.Time `gorm:"autoCreateTime"`
+	OriginAt        time.Time `gorm:"autoCreateTime"`
+}
+
+type CollectionAPI struct {
+	UUID            *uuid.UUID `json:"uuid,omitempty"`
+	CollectionIndex int64      `json:"index" binding:"required"`
+	Longtitude      float64    `json:"long" binding:"required"`
+	Latitude        float64    `json:"lat" binding:"required"`
+	Altitude        float64    `json:"alt" binding:"required"`
+	Accuracy        float64    `json:"acc" binding:"required"`
+	OriginAt        time.Time  `json:"origin_at,omitempty"`
 }
