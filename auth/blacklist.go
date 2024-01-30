1d4abb1906a1dea1b5aa0ac2e41f328b3f35af3f
devhoodit <hoodit@naver.com>
2024-01-30 17:53:21 +0900

    auth: set blacklist methods

--- auth/blacklist.go
+++ auth/blacklist.go
@@ -1,5 +1,41 @@
 package auth
 
+import (
+	"errors"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+)
+
 func (a *Auth) IsBlacklist(token string) (bool, error) {
 	return a.Store.IsBlacklist(token)
+}
+
+func (a *Auth) SetBlacklistByToken(claims *AuthClaims) error {
+	tokenString, err := a.generateToken(claims)
+	if err != nil {
+		return err
+	}
+	return a.DB.IfTokenExistRemoveElseErr(tokenString, time.Until(claims.ExpiresAt.Time), a.Store.SetBlacklist)
+}
+
+func (a *Auth) SetBlacklistByUserUUID(userUUID *uuid.UUID) error {
+	return nil
+}
+
+var ErrInValidRequest = errors.New("in valid request cannot find token")
+
+func TokenFromRequest(req *http.Request) (string, error) {
+	authString := req.Header.Get("Authorization")
+	authStruct := strings.Split(authString, " ")
+	if len(authStruct) != 2 {
+		return "", ErrInValidRequest
+	}
+	authScheme, authParam := authStruct[0], authStruct[1]
+	if authScheme != "Bearer" {
+		return "", ErrInValidRequest
+	}
+	return authParam, nil
 }
