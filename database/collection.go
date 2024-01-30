84b6c5ef5a498549edc2ad64862a3d7e80f3ebb3
devhoodit <hoodit@naver.com>
2024-01-30 17:56:33 +0900

    refac: use model api

--- database/collection.go
+++ database/collection.go
@@ -5,22 +5,31 @@ import (
 	"github.com/google/uuid"
 )
 
-func (d *Database) PutCollectionFromImageQueue(m *model.ImageQueue, index int64) error {
-	collection := &model.Collection{
-		UUID:            m.UUID,
-		UserUUID:        m.UserUUID,
-		Longtitude:      m.Altitude,
-		Latitude:        m.Latitude,
-		Altitude:        m.Altitude,
-		Accuracy:        m.Accuracy,
-		OriginAt:        m.CreatedAt,
-		CollectionIndex: index,
-	}
-	return d.DB.Create(collection).Error
-}
-
-func (d *Database) GetCollectection(userUUID *uuid.UUID, offset int, limit int) (*[]model.Collection, error) {
-	collections := []model.Collection{}
+func (d *DB) GetCollections(userUUID *uuid.UUID, offset int, limit int) (*[]model.CollectionAPI, error) {
+	collections := []model.CollectionAPI{}
 	err := d.DB.Where("user_uuid = ?", userUUID.String()).Offset(offset).Limit(limit).Find(&collections).Error
 	return &collections, err
+}
+
+func (d *DB) GetCollectionByUUID(collectionUUID *uuid.UUID) (collection *model.CollectionAPI, err error) {
+	err = d.DB.Where("uuid = ?", collectionUUID).Find(collection).Error
+	return
+}
+
+func (d *DB) CreateCollectionWithUserUUID(collection *model.CollectionAPI, userUUID *uuid.UUID) (collectionUUID *uuid.UUID, err error) {
+	uuid, err := uuid.NewRandom()
+	if err != nil {
+		return
+	}
+	collectionUUID = &uuid
+	err = d.DB.Create(&model.Collection{
+		UserUUID:        *userUUID,
+		UUID:            uuid,
+		CollectionIndex: collection.CollectionIndex,
+		Longtitude:      collection.Longtitude,
+		Latitude:        collection.Latitude,
+		Altitude:        collection.Altitude,
+		Accuracy:        collection.Accuracy,
+	}).Error
+	return
 }
