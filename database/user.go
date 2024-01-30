d633523f8c2de2bdf8451745a6c273bdd4e07eb2
devhoodit <hoodit@naver.com>
2024-01-30 17:57:29 +0900

    refac account, email

--- database/user.go
+++ database/user.go
@@ -1,22 +1,48 @@
 package database
 
 import (
-	"github.com/capdale/was/auth"
+	"errors"
+	"fmt"
+
 	"github.com/capdale/was/model"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
-func (d *Database) GetUserByEmail(email string) (*model.User, error) {
-	user := &model.User{}
-	err := d.DB.Where("email = ?", email).First(user).Error
+var ErrNoUserExist = errors.New("no user exists")
+
+func (d *DB) GetUserByEmail(email string) (user *model.User, err error) {
+	user = &model.User{}
+	err = d.DB.Where("email = ?", &email).First(user).Error
+	return
+}
+
+func (d *DB) IsUserUUIDExist(uuid *uuid.UUID) (err error) {
+	user := new(model.User)
+	err = d.DB.First(user, "uuid = ?", uuid.String()).Error
+	if err == gorm.ErrRecordNotFound {
+		return ErrNoUserExist
+	}
+	return
+}
+
+func (d *DB) CreateWithGithub(username string, email string) (*model.User, error) {
+	uuid, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+	user := &model.User{
+		Username: username,
+		UUID:     uuid,
+		Email:    email,
+	}
+	fmt.Print(user)
+	err = d.DB.Create(user).Error
 	return user, err
 }
 
-func (d *Database) CreateSocial(user *model.User) error {
-	return d.DB.Create(user).Error
-}
-
-func (d *Database) SetToken(claim *auth.AuthClaims) error {
-	return d.DB.Create(&model.Token{
-		UUID: claim.UserUUID,
-	}).Error
+func (d *DB) GetUsernameByUUID(uuid string) (*model.User, error) {
+	user := &model.User{}
+	err := d.DB.Select("username").First(user, "uuid = ?", uuid).Error
+	return user, err
 }
