b96430afb1f52f3b993cf140e0316c78301d3fd7
devhoodit <hoodit@naver.com>
2024-02-19 04:46:52 +0900

    auth token store use id instead of uuid

--- auth/auth.go
+++ auth/auth.go
@@ -4,11 +4,13 @@ import (
 	"time"
 
 	"github.com/capdale/was/model"
+	"github.com/capdale/was/types/binaryuuid"
 	"github.com/google/uuid"
 )
 
 type database interface {
-	SaveToken(userUUID *uuid.UUID, tokenString string, refreshToken *[]byte, agent *string) error
+	GetUserIdByUUID(userUUID binaryuuid.UUID) (int64, error)
+	SaveToken(userId int64, tokenString string, refreshToken *[]byte, agent *string) error
 	IfTokenExistRemoveElseErr(tokenString string, until time.Duration, blackToken func(string, time.Duration) error) error
 	PopTokenByRefreshToken(refreshToken *[]byte, transactionF func(string) error) (tokenString *string, err error)
 	QueryAllTokensByUserUUID(userUUID *uuid.UUID) (*[]*model.Token, error)
